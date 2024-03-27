a0d19b3dc464399c747cccc1aa93db2d48feead2
elise <s1141823@student.hsleiden.nl>
2024-03-27 13:24:23 +0100

    changed colors & font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    colors: {
+      black: 'var(--color-black)',
+      darkgray: 'var(--color-darkgray)',
+      gray: 'var(--color-gray)',
+      white: 'var(--color-white)',
+      green: 'var(--color-green)',
+      orange: 'var(--color-orange)',
+      blue: 'var(--color-blue)',
+    },
+    extend: {
+      fontFamily: ["inter", "sans-serif"],
+    },
   },
   plugins: [],
 }
