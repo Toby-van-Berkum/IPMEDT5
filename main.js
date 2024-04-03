a4fdefa787c66d380a1a8c4e098505008063aebf
elise <s1141823@student.hsleiden.nl>
2024-04-03 09:47:10 +0200

    tailwind added styling

---
+++ main.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+  var toggleButton = document.getElementById("toggle-buttons-container").children;
+  toggleButton.addEventListener('click', function () {
+    console.log("test");
+  });
+});
+
