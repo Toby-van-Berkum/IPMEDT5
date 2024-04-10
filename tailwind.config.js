fe134deeb384a9e2c2fefe18ed2b54e8fc615c79
Toby-van-Berkum <s1131370@student.hsleiden.nl>
2024-04-10 12:42:28 +0200

    Edited Login Page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
     colors: {
-      black: 'var(--color-black)',
-      darkgray: 'var(--color-darkgray)',
-      gray: 'var(--color-gray)',
-      white: 'var(--color-white)',
-      green: 'var(--color-green)',
-      orange: 'var(--color-orange)',
-      blue: 'var(--color-blue)',
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.gray,
+      emerald: colors.emerald,
+      indigo: colors.indigo,
+      yellow: colors.yellow,
     },
     extend: {
       fontFamily: ["inter", "sans-serif"],
+      colors: {
+        black: 'var(--color-black)',
+        darkgray: 'var(--color-darkgray)',
+        gray: 'var(--color-gray)',
+        white: 'var(--color-white)',
+        green: 'var(--color-green)',
+        orange: 'var(--color-orange)',
+        blue: 'var(--color-blue)',
+      },
     },
   },
   variants: 'all',
