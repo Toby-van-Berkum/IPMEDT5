c58eb0ce7100ab0c9ac4e858efdcae6ccc2113e7
JVP11292021 <jessyvanpolanen@gmail.com>
2024-04-15 13:27:18 +0200

    Update lib.js

--- js/lib.js
+++ js/lib.js
@@ -1,15 +1,15 @@
-/**
- * Attaches an event listener to a given button element that triggers a custom function with an API path as argument when clicked.
- * @param {HTMLButtonElement} btn - The button element to attach the event listener to.
- * @param {string} apiPath - The API URL path to be passed as an argument to the custom function.
- * @param {function} func - The custom function to be executed when the button is clicked. It should accept the API path as its argument.
- */
 
 const apiKey = {
     access: '',
     refresh: '',
 }
 
+/**
+ * Attaches an event listener to a given button element that triggers a custom function with an API path as argument when clicked.
+ * @param {HTMLButtonElement} btn - The button element to attach the event listener to.
+ * @param {string} apiPath - The API URL path to be passed as an argument to the custom function.
+ * @param {function} func - The custom function to be executed when the button is clicked. It should accept the API path as its argument.
+ */
 function addApiEvent(btn, apiPath, func) {
     if (btn === null && apiPath === null && func === null) return;
     btn.addEventListener("click", function() {
